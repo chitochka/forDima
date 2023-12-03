1f79881802dc1ae61809b5ea69fddd002e3c13aa
chitochka <javascript@post.cz>
2023-12-03 01:33:58 +0100

    render recors list

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,58 +1,58 @@
-
 const HABIT_KEY = 'HABIT_KEY';
-const ls = localStorage
-if (!ls.getItem(HABIT_KEY)) {
-    ls.setItem(HABIT_KEY, JSON.stringify(
+(function(){
+    const ls = localStorage
+    if (!ls.getItem(HABIT_KEY)) {
+        ls.setItem(HABIT_KEY, JSON.stringify(
 
-        [
-            {
-                id: 1,
-                active: true,
-                icon: "sport",
-                name: "kliky",
-                target: 10,
-                image: "dumbbell.svg",
-                days: [
-                    { comment: "Nějaký popis" },
-                    { comment: "Něco navíc" }
-                ]
-            },
-            {
-                id: 2,
-                icon: "water",
-                name: "volný čas",
-                target: 10,
-                image: "water.svg",
-                days: [
-                    { comment: "Výborně" }
-                ]
-            },
-            {
-                id: 3,
-                icon: "food",
-                name: "zdravá výživa",
-                target: 10,
-                image: "food.svg",
-                days: [
-                    { comment: "Výborně" },
-                    { comment: "Výborně 2" },
-                    { comment: "Výborně 3" }
-                ]
-            },
-            {
-                id: 4,
-                icon: "add",
-                name: "přidat koniček",
-                target: 10,
-                image: "add.svg",
-                days: [
-                    { comment: "Výborně" }
-                ]
-            }
-        ]
-            
-    ));
-}
+            [
+                {
+                    id: 1,
+                    active: true,
+                    icon: "sport",
+                    name: "kliky",
+                    target: 10,
+                    image: "dumbbell.svg",
+                    days: [
+                        { comment: "Nějaký popis" },
+                        { comment: "Něco navíc" }
+                    ]
+                },
+                {
+                    id: 2,
+                    icon: "water",
+                    name: "volný čas",
+                    target: 10,
+                    image: "water.svg",
+                    days: [
+                        { comment: "Výborně" }
+                    ]
+                },
+                {
+                    id: 3,
+                    icon: "food",
+                    name: "zdravá výživa",
+                    target: 10,
+                    image: "food.svg",
+                    days: [
+                        { comment: "Výborně" },
+                        { comment: "Výborně 2" },
+                        { comment: "Výborně 3" }
+                    ]
+                },
+                {
+                    id: 4,
+                    icon: "add",
+                    name: "přidat koniček",
+                    target: 10,
+                    image: "add.svg",
+                    days: [
+                        { comment: "Výborně" }
+                    ]
+                }
+            ]
+                
+        ));
+    }
 
-
+})()
 
--- assets/js/js.js
+++ assets/js/js.js
@@ -1,4 +1,6 @@
 window.onload = function () {
+    
+
     let ls = localStorage
     let arrData  = JSON.parse(ls.getItem(HABIT_KEY))
 
@@ -7,11 +9,12 @@ window.onload = function () {
         contentName : document.querySelector('#content-name'),
         progressBar : document.querySelector('.progress-bar-wrapper'),
         progressPercent : document.querySelector('.progress-percent'),
+        records : document.querySelector('ul.records'),
     }
 
     const renderMenu =()=>{
-        let arrItems =  arrData.map((item, index)=>{ return `<button class="aside-menu-item ${index?"":"active"}" data-id="${index+1}" menu-item-id="${index+1}"></button>`})
-        app.nav.innerHTML = arrItems.join(" ")
+        let arrHTML =  arrData.map((item, index)=>{ return `<button class="aside-menu-item ${index?"":"active"}" data-id="${index+1}" menu-item-id="${index+1}"></button>`})
+        app.nav.innerHTML = arrHTML.join(" ")
         app.nav.onclick = (e) =>{
             let element = e.target
             if (element.tagName.toLowerCase() !=="button") return
@@ -19,7 +22,7 @@ window.onload = function () {
              for (let i=0; i<navItems.length;i++){
                 navItems[i] === element
                     ? navItems[i].classList.add('active')
-                    :navItems[i].classList.remove('active')
+                    : navItems[i].classList.remove('active')
              }
             renderContent(element.dataset.id)
         }
@@ -28,11 +31,36 @@ window.onload = function () {
 
    const renderContent = (id) => {
        let dataObj = arrData.find( item => item.id == id)
-       app.contentName.innerText = dataObj.name
        const percent = dataObj.days.length*10
+       app.contentName.innerText = dataObj.name
        app.progressBar.style.cssText = `width: ${percent}%`
        app.progressPercent.innerText = percent + "%"   
+       renderRecordsList(dataObj.days)
     }
+    
+    const renderRecordsList =(days)=>{
+        let arrHTML =  days.map((day, index)=>{ 
+            return `
+            <li class="record">
+              <span class="record-day">Den ${index+1}.</span>
+              <span class="record-text">${day.comment}</span>
+              <button class="record-remove"></button>
+            </li>`
+        })
+        if (days.length < 10 ) arrHTML.push(inputTemplate(days.length +1))
+        app.records.innerHTML = arrHTML.join(" ")
+    }
+
+    const inputTemplate = (dayNumber) => { 
+        return `
+         <li class="record">
+            <span class="record-day">Den ${dayNumber}.</span>
+            <form class="record-add">
+                <input class="input-icon" type="text" placeholder="Komentář" />
+                <button>Přidat</button>
+            </form>
+        </li>
+    `}
 
 
     renderMenu()
